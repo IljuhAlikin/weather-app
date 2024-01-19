8339189d83920037a9c545b552e71d95e49b74b3
Ilya <iljuha.alikin@gmail.com>
2024-01-19 19:51:05 +0500

    added info into weather card

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
       <Search @place-data="addPlace" />
     </div>
     <div>
-      <p v-for="place in places">{{ place.location.name }}</p>
+      <!-- <p v-for="place in places">{{ place.location.name }}</p> -->
+      <div class="" v-for="(place, index) in places" :key="index">
+        <WeatherCard :place="place" />
+      </div>
     </div>
   </main>
 </template>
@@ -14,11 +17,14 @@ import { ref } from "vue";
 
 import Header from "./components/Header.vue";
 import Search from "./components/Search.vue";
+import WeatherCard from "./components/WeatherCard.vue";
 
 const places = ref([]);
 
 const addPlace = (data) => {
   places.value.push(data);
 };
+
+console.log(places);
 </script>
 <style scoped></style>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <p>{{ new Date(place.location.localtime).getHours() }}:</p>
+    <p>{{ new Date(place.location.localtime).getMinutes() }}</p>
+    {{ place.location.name }}
+    <img :src="place.current.condition.icon" alt="" />
+    <p>{{ Math.round(place.current.temp_c) }}&deg;</p>
+    <p>
+      {{ place.current.condition.text }}
+    </p>
+  </div>
+</template>
+<script setup>
+defineProps({
+  place: Object,
+});
+</script>
+<style scoped></style>
